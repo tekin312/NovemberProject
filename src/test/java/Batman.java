eec2c0bfaa5929da8298c703cd7153f793e802cb
tekin312 <nkulaksiz@gmail.com>
2020-11-01 10:16:16 -0600

    Gotham 's 6th commit

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -1,6 +1,7 @@
 public class Batman {
     public static void main(String[] args) {
         System.out.println("I will cat you Joker");
+        System.out.println("You've gotta be kidding me");
     }
 
 }
